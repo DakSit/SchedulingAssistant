4da75d70f03a122d8182d0859fc1828d2ac5c42a
Dak Sit <dakota.sitton@powerserve.net>
2021-10-08 00:25:41 -0500

    added two tables to initial db setup

---
+++ SchedulingAssistantDBO/dbo/Tables/Shift.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Shift] (
+    [Id]         INT           NOT NULL,
+    [Shift_Name] NVARCHAR (50) NOT NULL,
+    [Day]        NVARCHAR (50) NOT NULL,
+    [Start_Hour] NVARCHAR (50) NOT NULL,
+    [End_Hour]   NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Shift] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ SchedulingAssistantDBO/dbo/Tables/User.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[User] (
+    [ID]           INT          NOT NULL,
+    [Username]     VARCHAR (50) NOT NULL,
+    [First_Name]   VARCHAR (50) NOT NULL,
+    [Last_Name]    VARCHAR (50) NOT NULL,
+    [Password]     VARCHAR (50) NOT NULL,
+    [Email]        VARCHAR (50) NULL,
+    [Phone_Number] VARCHAR (50) NULL,
+    [IsActive]     BIT          NULL,
+    [Availability] INT          NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
