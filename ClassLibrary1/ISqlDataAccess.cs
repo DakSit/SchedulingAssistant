080909b44ea2dbb9ac8b89023a4ffc0299b1d020
Dean <deanthompson11@protonmail.com>
2021-10-20 20:06:28 -0500

    SQL data access. Connecting SQL to C#.

---
+++ ClassLibrary1/ISqlDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlData
+{
+    class ISqlDataAccess
+    {
+        public interface ISqlDataAccess
+        {
+            string ConnectionStringName { get; set; }
+
+            Task<List<T>> LoadData<T, U>(string sql, U parameters);
+            Task SaveData<T>(string sql, T parameters);
+        }
+    }
+}
---
+++ ClassLibrary1/UserData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlData
+{
+    class UserData
+    {
+        public class UserData
+        {
+            private readonly ISqlDataAccess _db;
+            public UserData(ISqlDataAccess db)
+            {
+                _db = db;
+            }
+
+            public Task<List<UserModel>> GetUsers()
+            {
+                string sql = "select * from dbo.Users";
+            }
+        }
+    }
+}
