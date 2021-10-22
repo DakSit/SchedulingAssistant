ff48cd309ce97c16ce5db857f74f3e20aea3d670
Dean <deanthompson11@protonmail.com>
2021-10-22 05:03:42 -0500

    Fixed the UserData pages. They actually work now!

---
+++ DataAccessLibrary/UsersData.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class UsersData
+    {
+        private readonly ISqlDataAccess _db;
+
+        public UsersData(ISqlDataAccess _db)
+        {
+            this._db = _db;
+        }
+
+        public Task<List<UserModel>> GetUsers()
+        {
+            string sql = "select * from dbo.Users";
+        }
+    }
+}
